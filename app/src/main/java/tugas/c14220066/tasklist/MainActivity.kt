5c5e6aeccd5a5061b7c15b26a3b1d384f78c8d15
Weldante <justinharsono14@gmail.com>
2024-12-11 14:18:08 +0700

    Membuat tasklist dengan firebase (bisa tambah dan hapus sekarang)

--- app/src/main/java/tugas/c14220066/tasklist/MainActivity.kt
+++ app/src/main/java/tugas/c14220066/tasklist/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
@@ -12,20 +13,26 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import kotlin.math.tan
 import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
 
-    private var _nama : MutableList<String> = emptyList<String>().toMutableList()
-    private var _tanggal : MutableList<String> = emptyList<String>().toMutableList()
-    private var _deskripsi : MutableList<String> = emptyList<String>().toMutableList()
-    private var _selesai : MutableList<Boolean> = emptyList<Boolean>().toMutableList()
-    private var _favorit : MutableList<Boolean> = emptyList<Boolean>().toMutableList()
+    val db = Firebase.firestore
 
-    lateinit var sp : SharedPreferences
-    var jumlahFavorit : Int = 0
+//    private var _nama : MutableList<String> = emptyList<String>().toMutableList()
+//    private var _tanggal : MutableList<String> = emptyList<String>().toMutableList()
+//    private var _deskripsi : MutableList<String> = emptyList<String>().toMutableList()
+//    private var _selesai : MutableList<Boolean> = emptyList<Boolean>().toMutableList()
+//    private var _favorit : MutableList<Boolean> = emptyList<Boolean>().toMutableList()
+
+//    lateinit var sp : SharedPreferences
+//    var jumlahFavorit : Int = 0
 
     private var arTaskList = arrayListOf<Task>()
 
@@ -42,7 +49,7 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        sp = getSharedPreferences("dataSP", MODE_PRIVATE)
+//        sp = getSharedPreferences("dataSP", MODE_PRIVATE)
 //        val gsonInit = Gson()
 //        val editorInit = sp.edit()
 //        arTaskList.add(Task("Test", "12345", "testing", false, true))
@@ -55,18 +62,8 @@ class MainActivity : AppCompatActivity() {
 //        if (isiSP != null) arTaskList = gson.fromJson(isiSP, type)
 
         _rvTaskList = findViewById<RecyclerView>(R.id.rvTaskList)
-//        if (arTaskList.size == 0) {
-//            SiapkanData()
-//        } else {
-//            arTaskList.forEach {
-//                _nama.add(it.nama)
-//                _tanggal.add(it.tanggal)
-//                _deskripsi.add(it.deskripsi)
-//                _selesai.add(it.selesai)
-//            }
-//            arTaskList.clear()
-//        }
-        TambahData()
+
+//        TambahData()
         TampilkanData()
 
         val _btnTambah = findViewById<Button>(R.id.btnTambah)
@@ -79,130 +76,208 @@ class MainActivity : AppCompatActivity() {
         if (dataTask != null) {
             val position = intent.getParcelableExtra<Int>("Position", Int::class.java)
             if (position != null) {
-                _nama.set(position, dataTask.nama)
-                _tanggal.set(position, dataTask.tanggal)
-                _deskripsi.set(position, dataTask.deskripsi)
-                _selesai.set(position, dataTask.selesai)
-                _favorit.set(position, dataTask.favorit)
-                TambahData()
+//                _nama.set(position, dataTask.nama)
+//                _tanggal.set(position, dataTask.tanggal)
+//                _deskripsi.set(position, dataTask.deskripsi)
+//                _selesai.set(position, dataTask.selesai)
+//                _favorit.set(position, dataTask.favorit)
+//                TambahData()
                 TampilkanData()
             }
             else {
-                _nama.add(dataTask.nama)
-                _tanggal.add(dataTask.tanggal)
-                _deskripsi.add(dataTask.deskripsi)
-                _selesai.add(dataTask.selesai)
-                _favorit.add(dataTask.favorit)
-                TambahData()
+                TambahData(db, dataTask.nama, dataTask.tanggal, dataTask.deskripsi, dataTask.selesai,
+                    dataTask.favorit)
+//                _nama.add(dataTask.nama)
+//                _tanggal.add(dataTask.tanggal)
+//                _deskripsi.add(dataTask.deskripsi)
+//                _selesai.add(dataTask.selesai)
+//                _favorit.add(dataTask.favorit)
+//                TambahData()
                 TampilkanData()
             }
         }
 
     }
 
-    fun SiapkanData () {
-        _nama = mutableListOf<String>("Tugas PABA", "hebat")
-        _tanggal = mutableListOf<String>("20 Desember 2024", "20 Agustus")
-        _deskripsi = mutableListOf<String>("Lihat Classroom", "Test")
-        _selesai = mutableListOf<Boolean>(true, false)
+//    fun SiapkanData () {
+//        _nama = mutableListOf<String>("Tugas PABA", "hebat")
+//        _tanggal = mutableListOf<String>("20 Desember 2024", "20 Agustus")
+//        _deskripsi = mutableListOf<String>("Lihat Classroom", "Test")
+//        _selesai = mutableListOf<Boolean>(true, false)
+//    }
+
+//    untuk sharedpreference
+//    fun TambahData() {
+//        arTaskList.clear()
+//        val gson = Gson()
+//        val isiSP = sp.getString("spTask", null)
+//        val type = object : TypeToken<ArrayList<Task>>() {}.type
+//        if (isiSP != null) arTaskList = gson.fromJson(isiSP, type)
+//        jumlahFavorit = arTaskList.size
+//        for (position in _nama.indices) {
+//            val data = Task(
+//                _nama[position],
+//                _tanggal[position],
+//                _deskripsi[position],
+//                _selesai[position],
+//                _favorit[position]
+//            )
+//            arTaskList.add(data)
+//        }
+//
+//    }
+
+    fun TambahData(db: FirebaseFirestore, nama: String, tanggal: String, deskripsi: String,
+                   selesai: Boolean, favorit: Boolean) {
+        val dataBaru = Task(nama, tanggal, deskripsi, selesai, favorit)
+        db.collection("tbTask")
+            .document(dataBaru.nama)
+            .set(dataBaru)
+            .addOnSuccessListener {
+                Log.d("Firebase", "Data Berhasil Disimpan")
+            }
+            .addOnFailureListener {
+                Log.d("Firebase", it.message.toString())
+            }
     }
 
-    fun TambahData() {
-        arTaskList.clear()
-        val gson = Gson()
-        val isiSP = sp.getString("spTask", null)
-        val type = object : TypeToken<ArrayList<Task>>() {}.type
-        if (isiSP != null) arTaskList = gson.fromJson(isiSP, type)
-        jumlahFavorit = arTaskList.size
-//        val isiJumlah = sp.getString("jumlah", null)
-//        val jumlahType = object : TypeToken<Int>() {}.type
-//        if (isiJumlah != null) jumlahFavorit = gson.fromJson<Int>(isiJumlah, jumlahType).toString().toInt()
-        for (position in _nama.indices) {
-            val data = Task(
-                _nama[position],
-                _tanggal[position],
-                _deskripsi[position],
-                _selesai[position],
-                _favorit[position]
-            )
-            arTaskList.add(data)
-        }
+    fun ReadData(db:FirebaseFirestore) {
+        db.collection("tbTask").get()
+            .addOnSuccessListener {
+                result ->
+                arTaskList.clear()
+                for (task in result) {
+                    val readData = Task(
+                        task.data.get("nama").toString(),
+                        task.data.get("tanggal").toString(),
+                        task.data.get("deskripsi").toString(),
+                        task.data.get("selesai").toString().toBoolean(),
+                        task.data.get("favorit").toString().toBoolean()
+                        )
+                    arTaskList.add(readData)
+                }
+                _rvTaskList.layoutManager = LinearLayoutManager(this)
+                val adapterTaskList = AdapterRecView(arTaskList)
+                _rvTaskList.adapter = adapterTaskList
 
+                adapterTaskList.setOnItemClickCallback(object : AdapterRecView.OnItemClickCallback{
+                    override fun delData(position: Int) {
+                        val namaTask = arTaskList[position].nama
+                        db.collection("tbTask")
+                            .document(namaTask)
+                            .delete()
+                            .addOnSuccessListener {
+                                Log.d("Firebase", "Berhasil dihapus")
+                                TampilkanData()
+                            }
+                            .addOnFailureListener { e ->
+                                Log.w("Firebase", e.message.toString())
+                            }
+                    }
+
+                    override fun editData(data: Task, position: Int) {
+                        val intent = Intent(this@MainActivity, Simpan::class.java)
+                        intent.putExtra("DataTask", data)
+                        intent.putExtra("Position", position)
+                        startActivity(intent)
+                    }
+
+                    override fun taskSelesai(position: Int) {
+                        TampilkanData()
+                    }
+
+                    override fun favorit(position: Int) {
+                        TampilkanData()
+                    }
+
+                    @RequiresApi(35)
+                    override fun unFavorit(position: Int) {
+                        TampilkanData()
+                    }
+
+                })
+            }
+            .addOnFailureListener {
+                Log.d("Firebase", it.message.toString())
+            }
     }
 
     fun TampilkanData() {
-        _rvTaskList.layoutManager = LinearLayoutManager(this)
-        val adapterTaskList = AdapterRecView(arTaskList)
-        _rvTaskList.adapter = adapterTaskList
-
-        adapterTaskList.setOnItemClickCallback(object : AdapterRecView.OnItemClickCallback{
-            override fun delData(position: Int) {
-                val position = position - jumlahFavorit
-                _nama.removeAt(position)
-                _tanggal.removeAt(position)
-                _deskripsi.removeAt(position)
-                _selesai.removeAt(position)
-                _favorit.removeAt(position)
-                TambahData()
-                TampilkanData()
-            }
-
-            override fun editData(data: Task, position: Int) {
-                val position = position - jumlahFavorit
-                val intent = Intent(this@MainActivity, Simpan::class.java)
-                intent.putExtra("DataTask", data)
-                intent.putExtra("Position", position)
-                startActivity(intent)
-            }
-
-            override fun taskSelesai(position: Int) {
-                val position = position - jumlahFavorit
-                _selesai.set(position, true)
-                TambahData()
-                TampilkanData()
-            }
-
-            override fun favorit(position: Int) {
-                arTaskList[position].favorit = true
-                val gson = Gson()
-                val isiSP = sp.getString("spTask", null)
-                val type = object : TypeToken<ArrayList<Task>>() {}.type
-                if (isiSP != null) {
-                    var newTaskList = gson.fromJson<ArrayList<Task>>(isiSP, type)
-                    newTaskList.add(arTaskList[position])
-                    val editor = sp.edit()
-                    val json = gson.toJson(newTaskList)
-                    editor.putString("spTask", json)
-                    editor.putString("jumlah", json)
-                    editor.apply()
-                }
-                TambahData()
-                TampilkanData()
-            }
-
-            @RequiresApi(35)
-            override fun unFavorit(position: Int) {
-                val gson = Gson()
-                val isiSP = sp.getString("spTask", null)
-                val type = object : TypeToken<ArrayList<Task>>() {}.type
-                if (isiSP != null) {
-                    var newTaskList = gson.fromJson<ArrayList<Task>>(isiSP, type)
-                    newTaskList.remove(arTaskList[position])
-                    val editor = sp.edit()
-                    val json = gson.toJson(newTaskList)
-                    editor.putString("spTask", json)
-                    editor.apply()
-                }
-                _nama.addFirst(arTaskList[position].nama)
-                _tanggal.addFirst(arTaskList[position].tanggal)
-                _deskripsi.addFirst(arTaskList[position].deskripsi)
-                _selesai.addFirst(arTaskList[position].selesai)
-                _favorit.addFirst(false)
-                TambahData()
-                TampilkanData()
-            }
-
-        })
+        ReadData(db)
+//        _rvTaskList.layoutManager = LinearLayoutManager(this)
+//        val adapterTaskList = AdapterRecView(arTaskList)
+//        _rvTaskList.adapter = adapterTaskList
+//
+//        Log.i("Help", "Bisa")
+//
+//        adapterTaskList.setOnItemClickCallback(object : AdapterRecView.OnItemClickCallback{
+//            override fun delData(position: Int) {
+////                val position = position - jumlahFavorit
+////                _nama.removeAt(position)
+////                _tanggal.removeAt(position)
+////                _deskripsi.removeAt(position)
+////                _selesai.removeAt(position)
+////                _favorit.removeAt(position)
+////                TambahData()
+//                TampilkanData()
+//            }
+//
+//            override fun editData(data: Task, position: Int) {
+////                val position = position - jumlahFavorit
+//                val intent = Intent(this@MainActivity, Simpan::class.java)
+//                intent.putExtra("DataTask", data)
+//                intent.putExtra("Position", position)
+//                startActivity(intent)
+//            }
+//
+//            override fun taskSelesai(position: Int) {
+////                val position = position - jumlahFavorit
+////                _selesai.set(position, true)
+////                TambahData()
+//                TampilkanData()
+//            }
+//
+//            override fun favorit(position: Int) {
+////                arTaskList[position].favorit = true
+////                val gson = Gson()
+////                val isiSP = sp.getString("spTask", null)
+////                val type = object : TypeToken<ArrayList<Task>>() {}.type
+////                if (isiSP != null) {
+////                    var newTaskList = gson.fromJson<ArrayList<Task>>(isiSP, type)
+////                    newTaskList.add(arTaskList[position])
+////                    val editor = sp.edit()
+////                    val json = gson.toJson(newTaskList)
+////                    editor.putString("spTask", json)
+////                    editor.putString("jumlah", json)
+////                    editor.apply()
+////                }
+////                TambahData()
+//                TampilkanData()
+//            }
+//
+//            @RequiresApi(35)
+//            override fun unFavorit(position: Int) {
+////                val gson = Gson()
+////                val isiSP = sp.getString("spTask", null)
+////                val type = object : TypeToken<ArrayList<Task>>() {}.type
+////                if (isiSP != null) {
+////                    var newTaskList = gson.fromJson<ArrayList<Task>>(isiSP, type)
+////                    newTaskList.remove(arTaskList[position])
+////                    val editor = sp.edit()
+////                    val json = gson.toJson(newTaskList)
+////                    editor.putString("spTask", json)
+////                    editor.apply()
+////                }
+////                _nama.addFirst(arTaskList[position].nama)
+////                _tanggal.addFirst(arTaskList[position].tanggal)
+////                _deskripsi.addFirst(arTaskList[position].deskripsi)
+////                _selesai.addFirst(arTaskList[position].selesai)
+////                _favorit.addFirst(false)
+////                TambahData()
+//                TampilkanData()
+//            }
+//
+//        })
     }
 
 
--- app/src/main/java/tugas/c14220066/tasklist/Simpan.kt
+++ app/src/main/java/tugas/c14220066/tasklist/Simpan.kt
@@ -43,7 +43,6 @@ class Simpan : AppCompatActivity() {
                 intent.putExtra("DataTask", data)
                 intent.putExtra("Position", position)
                 startActivity(intent)
-
             }
         }
         else {
